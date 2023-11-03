b53d9b4608233b5aba34b36b40360a76efdd3302
Ben <manglebot@users.noreply.github.com>
2023-11-03 23:41:00 +0000

    fix:roman numerals code cleanup

--- Kata_1_roman_numerals.test.ts
+++ Kata_1_roman_numerals.test.ts
@@ -19,33 +19,3 @@ test("Return DCCLV from 755", () => {
 test("Return MMLXXXVII from 2087", () => {
   expect(romanNumerals(2087)).toEqual('MMLXXXVII');
 });
-
-
-
-
-// I = 1
-// V = 5
-// X = 10
-// L = 50
-// C = 100
-// D = 500
-// M = 1000
-
-// letters can only be use 3 times in a row and values are added
-// 
-// eg III = 3, XXX = 30, CCC = 300 etc
-
-// adding XI = 11, LV = 55, CL = 150, DC = 600
-// LXVI = 66, CCLX = 250, DCCLV = 755
-
-// subtracking - if numeral is smaller we subtract
-// eg IX = 9
-
-// test ideas... convert numverals to numbers.
-// try adding numerals.
-// for minus - try - if numeral less than next numberal, subtract.
-
-// needs to be the opposite way around - return numeral
-
-
-//later = parameterised testing
--- Kata_1_roman_numerals.ts
+++ Kata_1_roman_numerals.ts
@@ -1,23 +1,3 @@
-
-
-// const mainNumerals: Object{} string: number > = [
-// wrong way round!!
-    // { value:1, numeral:'I' },
-    // { value:4, numeral:'IV' },
-    // { value:5, numeral:'V' },
-    // { value:9, numeral:'IX' },
-    // { value:10, numeral:'X' },
-    // { value:40, numeral:'XL' },
-    // { value:50, numeral:'L' },
-    // { value:90, numeral:'LC' },
-    // { value:100, numeral:'C' },
-    // { value:400, numeral:'CD' },
-    // { value:500, numeral:'D' },
-    // { value:900, numeral:'CM' },
-    // { value:1000, numeral:'M' }
-
-    // ];
-
     const mainNumerals: Record<string, number> = {
         M: 1000,
         CM: 900,
@@ -34,60 +14,16 @@
         I: 1,
     };
 
-
-
 export function romanNumerals(num:number):string {
 
-    let roman = '';
+    let romanNunerals = '';
 
-    let test = {mainNumerals}
-        for ( let i in mainNumerals ) {
-            console.log("mainNumerals[i] = "+mainNumerals[i]);
-            while ( num >= mainNumerals[i] ) {
-              roman += i;
-              num -= mainNumerals[i];
-            }
-        };
-
-            // console.log(test);
-
+    for ( const i in mainNumerals ) {
+        while ( num >= mainNumerals[i] ) {
+            romanNunerals += i;
+            num -= mainNumerals[i];
+        }
+    };
     
-    // split the number into 1's, 10's and 100's and 1000's
-    // 123 = 100, 20, 3
-    // CXXIII
-    // 287   200 80 7
-    // CCLXXXVII
-
-    // const test = splitToDigit(num);
-    // const splitNumberChunks: number[] = [];
-    
-    // function splitToDigit(n: number){
-    //         return (n + '').split('').map((i) => { return Number(i); })
-    //       }
-          
-    // let multiplier = 1;
-          
-    // for (let i=test.length -1; i >= 0; i--){
-    //     splitNumberChunks.unshift(test[i]* multiplier);
-    //       multiplier *= 10;
-    //         console.log(splitNumberChunks);
-    //       };
-    //     //   this returns an array. need to turn into roman
-
-    //     splitNumberChunks.forEach(element => {
-    //         if(element of splitNumberChunks >){
-    //             // do something;
-    //         }
-    //     });
-
-        // I accidentally saw this answer on the web. it's so simple. ouch!
-        // for ( i in lookup ) {
-        //     while ( num >= lookup[i] ) {
-        //       roman += i;
-        //       num -= lookup[i];
-        //     }
-
-        // const output: string = 'X';
-
-    return roman;
+    return romanNunerals;
 };
