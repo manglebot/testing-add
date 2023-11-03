ea307afc98baf40d0eada6a11dac1f4b06c662fa
Ben <manglebot@users.noreply.github.com>
2023-11-03 13:51:36 +0000

    feat:add more tests. it works

--- Kata_1_roman_numerals.test.ts
+++ Kata_1_roman_numerals.test.ts
@@ -1,16 +1,27 @@
 import { romanNumerals } from "./Kata_1_roman_numerals";
 
-test("Return X", () => {
+test("Return X from 10", () => {
   expect(romanNumerals(10)).toEqual('X');
 });
 
-test("Return XX", () => {
+test("Return XX from 20", () => {
   expect(romanNumerals(20)).toEqual('XX');
 });
 
-test("Return XX", () => {
+test("Return LXVI from 66", () => {
   expect(romanNumerals(66)).toEqual('LXVI');
 });
+
+test("Return DCCLV from 755", () => {
+  expect(romanNumerals(755)).toEqual('DCCLV');
+});
+
+test("Return MMLXXXVII from 2087", () => {
+  expect(romanNumerals(2087)).toEqual('MMLXXXVII');
+});
+
+
+
 
 // I = 1
 // V = 5
