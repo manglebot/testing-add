d76c9b46afa5d89fe424b34ad1147b9c7cedb73a
Ben <manglebot@users.noreply.github.com>
2023-11-05 09:36:49 +0000

    fix:get frames updating with rolls
    desc:the frames are now updating with each roll, but scores aren't adding up yet

--- kata_2_bowling.ts
+++ kata_2_bowling.ts
@@ -4,23 +4,32 @@ export function bowlingScore(scores:string[]):number {
 
     // create 10 empty frames and put score into them. if strike or slice, start with 0 and add later.
     const frames = [
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 },
-        { roll1:null, roll2:null, score:0 }
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 },
+        { roll1:null as unknown, roll2:null as unknown, score:0 }
     ]
 
     scores.forEach((score,index) => {
-        //console.log("score: "+score);
+
+        const currentFrame = frames[index];
+
         if (Number(score) < 10){
+
+            if(currentFrame.roll1 === null){
+                currentFrame.roll1 = Number(score);
+            }
+            if(currentFrame.roll2 === null){
+                currentFrame.roll2 = Number(score);
+            }
             // push number to frames thingy...
-            frames[index].score += Number(score);
+            //frames[index].score += Number(score);
             console.log(frames);
         }
         // creat else rules for strikes etc.
