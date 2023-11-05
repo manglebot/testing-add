6ac5b94c809b00189a5594ff5490e1ebf8b8fca0
Ben <manglebot@users.noreply.github.com>
2023-11-05 10:29:30 +0000

    fix:push rolls into each frame
    desc:it's duplicating each roll. need to fix

--- kata_2_bowling.test.ts
+++ kata_2_bowling.test.ts
@@ -1,8 +1,13 @@
 import { bowlingScore } from "./kata_2_bowling";
 
-test("Return 33 from 3 rolls", () => {
+test("Return 12 from 3 rolls", () => {
   expect(bowlingScore(["4","4","4"])).toBe(12);
 });
+
+// for this test the
+test("Return 10 from 4 rolls (ie 2 frames)", () => {
+    expect(bowlingScore(["1","2","3","4"])).toBe(18);
+  });
 
 // test("Return 6 from 3 rolls of 1", () => {
 //     expect(bowlingScore([2,1,3,1,4,1])).toBe(12);
--- kata_2_bowling.ts
+++ kata_2_bowling.ts
@@ -3,17 +3,21 @@ export function bowlingScore(scores:string[]):number {
     // input is currently ["4","4","4"]
 
     // create 10 empty frames and put score into them. if strike or slice, start with 0 and add later.
+
+    // null as number creates a warning, but seems to work. Any hints welcome.
+    // I did try as unknown, but that created errors later - and I know that it's a number eventually.
+
     const frames = [
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 },
-        { roll1:null as unknown, roll2:null as unknown, score:0 }
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 },
+        { roll1:null as number, roll2:null as number, score:0 }
     ]
 
     scores.forEach((score,index) => {
@@ -25,18 +29,22 @@ export function bowlingScore(scores:string[]):number {
             if(currentFrame.roll1 === null){
                 currentFrame.roll1 = Number(score);
             }
-            if(currentFrame.roll2 === null){
+            if (currentFrame.roll1 !== null && currentFrame.roll2 === null){
                 currentFrame.roll2 = Number(score);
             }
-            // push number to frames thingy...
-            //frames[index].score += Number(score);
-            console.log(frames);
-        }
-        // creat else rules for strikes etc.
-    });
+            currentFrame.score += currentFrame.roll1 + currentFrame.roll2;
 
-    console.log("scores: "+scores)
+            } else {
+                // deal with strikes and spares here "#" "/"
+            }
+        });
+
+    const finalScore = frames.reduce((total, frames) => {
+        return total+frames.score;
+    }, 0);
+    
+    console.log(frames)
 
 
-    return 12;
+    return finalScore;
 };
